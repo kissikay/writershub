79b9c2d71f4e0011d66405d1399b5c3679ebe7f3
kissikay <stanleykissi7@gmail.com>
2025-10-15 20:50:05 +0000

    removed sensitive file

--- sec.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a 32-character random string
-const secretKey = crypto.randomBytes(16).toString('hex');
-
-console.log('Generated Secret Key:', secretKey);
